4e8e8b9264ef90b01d3a55e3b1e4629d703b25fd
ryoya <ryoya47@gmail.com>
2022-06-15 22:10:24 +0900

    freePlacement.dart 自由配置に複数widget ver2

--- lib/freePlacement.dart
+++ lib/freePlacement.dart
@@ -15,62 +15,85 @@ class freePlacement extends StatefulWidget {
 
 // ignore: camel_case_types
 class _freePlacementState extends State<freePlacement> {
-  Offset pos = Offset(50, 50);
-  Offset pos2 = Offset(200, 200);
+
+  List<Cont> _contList = <Cont>[];
+  late Cont _cont;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _cont = Cont(const Offset(50,50),50,50);
+    _contList.add(_cont);
+    _cont = Cont(const Offset(200,200),50,50);
+    _contList.add(_cont);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Positioned(
-            left: pos.dx,
-            top: pos.dy,
-            child: Draggable(
-              feedback: Container(
-                width: 50,
-                height: 50,
-                color: Colors.blue[100],
-              ),
-              child: Container(
-                width: 50,
-                height: 50,
-                color: Colors.blue,
-              ),
-              childWhenDragging: Container(),
-              // 追加部分
-              onDraggableCanceled: (view, offset) {
-                setState(() {
-                  pos = offset;
-                });
-              },
-            ),
+          Container(
+            alignment: Alignment(0.5, 0.0),
+            color: Colors.white70,
+            child: Text('Widget A'),
           ),
-          Positioned(
-            left: pos2.dx,
-            top: pos2.dy,
-            child: Draggable(
-              feedback: Container(
-                width: 50,
-                height: 50,
-                color: Colors.yellow[100],
-              ),
-              child: Container(
-                width: 50,
-                height: 50,
-                color: Colors.yellow,
-              ),
-              childWhenDragging: Container(),
-              // 追加部分
-              onDraggableCanceled: (view, offset) {
-                setState(() {
-                  pos2 = offset;
-                });
-              },
-            ),
+          Container(
+            alignment: Alignment(0.0, 0.5),
+            color: Colors.white70,
+            child: Text('Widget A'),
           ),
+          Container(
+            alignment: Alignment(0.0, 0.0),
+            color: Colors.white70,
+            child: Text('Widget A'),
+          ),
+          returnFreePositionContainer(_contList[0]),
+          returnFreePositionContainer(_contList[1]),
         ],
       ),
     );
   }
+
+  Widget returnFreePositionContainer(Cont _tempCont){
+    return Positioned(
+      left: _tempCont.pos.dx,
+      top: _tempCont.pos.dy,
+      child: Draggable(
+        feedback: Container(
+          width: _tempCont.width,
+          height: _tempCont.height,
+          color: Colors.blue[100],
+        ),
+        child: Container(
+          width: _tempCont.width,
+          height: _tempCont.height,
+          color: Colors.blue,
+        ),
+        childWhenDragging: Container(),
+        // 追加部分
+        onDraggableCanceled: (view, offset) {
+          setState(() {
+            _tempCont.pos = offset;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class Cont{
+
+  late Offset pos;
+  late double width;
+  late double height;
+
+  Cont(Offset a,double c, double d){
+    pos = a;
+    width = c;
+    height = d;
+  }
+
 }
