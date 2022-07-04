5dd5f192c950789e3198ac72cd123ba5b3cc80cd
ryoya <ryoya47@gmail.com>
2022-07-04 22:03:42 +0900

    自由座標の配置指定 一番下の段完了

--- lib/freePlacement.dart
+++ lib/freePlacement.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'config.dart';
 
 // ignore: camel_case_types
 class freePlacement extends StatefulWidget {
@@ -19,11 +18,6 @@ class _freePlacementState extends State<freePlacement> {
   List<Cont> _contList = <Cont>[];
   late Cont _cont;
 
-  //late double deviceHeight = MediaQuery.of(context).size.height;
-  //late double deviceWidth = MediaQuery.of(context).size.width;
-
-
-
   @override
   void initState() {
     super.initState();
@@ -31,51 +25,427 @@ class _freePlacementState extends State<freePlacement> {
     _contList.add(_cont);
     _cont = Cont(const Offset(200,200),50,50);
     _contList.add(_cont);
-    //print(deviceHeight);
-    //print(deviceWidth);
   }
 
   @override
   Widget build(BuildContext context) {
+    // デバイスの横サイズ
+    double _deviceWidth = MediaQuery.of(context).size.width;
+    // デバイスの縦サイズ
+    double _deviceHeight = MediaQuery.of(context).size.height;
+    // コンテナの1辺の長さ
+    double _containerSize = _deviceWidth * 0.2;
+    // バナー広告の大きさ
+    double _bannerHeight = _deviceWidth * 0.15;
+    // コンテナのスペース
+    double _space = _deviceWidth * 0.025;
+    // コンテナ一番サイドのスペース
+    double _sizeSpace = _deviceWidth * 0.125 / 2;
     return Scaffold(
       body: Stack(
         children: <Widget>[
           Container(
-            alignment: Alignment(0.0,-0.5),
-            child: SizedBox(
-              width:  70,
-              height: 70,
-              child: ElevatedButton(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    primary: Colors.blue.shade300
+            child: Positioned(
+              top: _deviceHeight - _containerSize - _bannerHeight,
+              left: _sizeSpace,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
                 ),
-                child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
               ),
             ),
           ),
           Container(
-            alignment: Alignment(0.0,-0.3),
-            child: SizedBox(
-              width:  70,
-              height: 70,
-              child: ElevatedButton(
-                onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    primary: Colors.blue.shade300
+            child: Positioned(
+              top: _deviceHeight - _containerSize - _bannerHeight,
+              left: _sizeSpace + _space * 1 + _containerSize * 1,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
                 ),
-                child: const Text('AAA',style: TextStyle(color: Colors.white,fontSize: 20),),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize - _bannerHeight,
+              left: _sizeSpace + _space * 2 + _containerSize * 2,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize - _bannerHeight,
+              left: _sizeSpace + _space * 3 + _containerSize * 3,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 2 - _bannerHeight - _space * 1,
+              left: _space * 1 + _containerSize * 0,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 2 - _bannerHeight - _space * 1,
+              left: _space * 2 + _containerSize * 1,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 2 - _bannerHeight - _space * 1,
+              left: _space * 3 + _containerSize * 2,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 2 - _bannerHeight - _space * 1,
+              left: _space * 4 + _containerSize * 3,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 3 - _bannerHeight - _space * 2,
+              left: _space * 1 + _containerSize * 0,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 3 - _bannerHeight - _space * 2,
+              left: _space * 2 + _containerSize * 1,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 3 - _bannerHeight - _space * 2,
+              left: _space * 3 + _containerSize * 2,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 3 - _bannerHeight - _space * 2,
+              left: _space * 4 + _containerSize * 3,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 4 - _bannerHeight - _space * 3,
+              left: _space * 1 + _containerSize * 0,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 4 - _bannerHeight - _space * 3,
+              left: _space * 2 + _containerSize * 1,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 4 - _bannerHeight - _space * 3,
+              left: _space * 3 + _containerSize * 2,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 4 - _bannerHeight - _space * 3,
+              left: _space * 4 + _containerSize * 3,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 5 - _bannerHeight - _space * 4,
+              left: _space * 1 + _containerSize * 0,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 5 - _bannerHeight - _space * 4,
+              left: _space * 2 + _containerSize * 1,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 5 - _bannerHeight - _space * 4,
+              left: _space * 3 + _containerSize * 2,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Positioned(
+              top: _deviceHeight - _containerSize * 5 - _bannerHeight - _space * 4,
+              left: _space * 4 + _containerSize * 3,
+              width: _containerSize,
+              height: _containerSize,
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      primary: Colors.blue.shade300
+                  ),
+                  child: const Text('AC',style: TextStyle(color: Colors.white,fontSize: 20),),
+                ),
               ),
             ),
           ),
           returnFreePositionContainer(_contList[0]),
-          returnFreePositionContainer(_contList[1]),
+          //returnFreePositionContainer(_contList[1]),
         ],
       ),
     );
